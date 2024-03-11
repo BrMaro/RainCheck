a85553353b9eb69bde3634f0cc981c9351f78c6a
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-03-11 18:34:13 +0300

    convert into df and save to csv file

---
+++ api_call.py
@@ -0,0 +1,47 @@
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+openmeteo = openmeteo_requests.Client(session=retry_session)
+
+WEATHER_DATA_FILE = "hourly_weather_data.csv"
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": -1.3802,
+    "longitude": 36.7463,
+    "hourly": ["precipitation_probability", "precipitation", "rain"],
+    "timezone": "auto"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_precipitation_probability = hourly.Variables(0).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+    start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+    end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+    freq=pd.Timedelta(seconds=hourly.Interval()),
+    inclusive="left"
+)}
+hourly_data["precipitation_probability"] = hourly_precipitation_probability
+# hourly_data["precipitation"] = hourly_precipitation
+# hourly_data["rain"] = hourly_rain
+
+hourly_dataframe = pd.DataFrame(data=hourly_data)
+print(hourly_dataframe)
+hourly_dataframe.to_csv(WEATHER_DATA_FILE,index=False)
