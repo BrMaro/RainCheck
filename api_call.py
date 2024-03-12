bbda620be17ce9b99013759f820bd3f22d66fb8c
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-03-12 15:00:30 +0300

    modified api to call for rain only

--- api_call.py
+++ api_call.py
@@ -1,21 +1,24 @@
 import openmeteo_requests
+
 import requests_cache
 import pandas as pd
 from retry_requests import retry
 
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
-retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-openmeteo = openmeteo_requests.Client(session=retry_session)
-
 WEATHER_DATA_FILE = "hourly_weather_data.csv"
 
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-    "latitude": -1.3802,
-    "longitude": 36.7463,
-    "hourly": ["precipitation_probability", "precipitation", "rain"],
-    "timezone": "auto"
+	"latitude": -1.3802,
+	"longitude": 36.7463,
+	"hourly": "rain"
 }
 responses = openmeteo.weather_api(url, params=params)
 
@@ -28,20 +31,18 @@ print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
 # Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
-hourly_precipitation_probability = hourly.Variables(0).ValuesAsNumpy()
-hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
-hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+hourly_rain = hourly.Variables(0).ValuesAsNumpy()
 
 hourly_data = {"date": pd.date_range(
-    start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-    end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-    freq=pd.Timedelta(seconds=hourly.Interval()),
-    inclusive="left"
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
 )}
-hourly_data["precipitation_probability"] = hourly_precipitation_probability
-# hourly_data["precipitation"] = hourly_precipitation
-# hourly_data["rain"] = hourly_rain
+hourly_data["rain"] = hourly_rain
 
-hourly_dataframe = pd.DataFrame(data=hourly_data)
+hourly_dataframe = pd.DataFrame(data = hourly_data)
 print(hourly_dataframe)
+
+
 hourly_dataframe.to_csv(WEATHER_DATA_FILE,index=False)
