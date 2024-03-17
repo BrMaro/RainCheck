c2f97a2fd00dcfd68cfb364c3121bf4726256fbb
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-03-17 19:41:36 +0300

    updating

--- messenger.py
+++ messenger.py
@@ -44,21 +44,24 @@ def main():
     current_datetime_str= datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     current_datetime = datetime.strptime(current_datetime_str, "%Y-%m-%d %H:%M:%S")
     print("Current date and time:", current_datetime)
-    # print(read_hourly_dataframe[:24])
 
-    moderate_rainy_hours = [hr[11:16] for hr in filter_dates_by_amount(read_hourly_dataframe[:24], THRESHOLD_AMOUNT,HIGH_RAINFALL)]
-    high_rainy_hours = [hr[11:16] for hr in filter_dates_by_amount(read_hourly_dataframe[:24], HIGH_RAINFALL, 100)]
+    moderate_rainy_hours = ", ".join([hr[11:16] for hr in filter_dates_by_amount(read_hourly_dataframe[:24], THRESHOLD_AMOUNT,HIGH_RAINFALL)])
+    high_rainy_hours = ", ".join([hr[11:16] for hr in filter_dates_by_amount(read_hourly_dataframe[:24], HIGH_RAINFALL, 100)])
 
     print(moderate_rainy_hours)
     print(high_rainy_hours)
 
-    if moderate_rainy_hours or high_rainy_hours:
-        message = f"Moderate chance of rainfall is expected at  hours {moderate_rainy_hours}\n\nHigh chance of rainfall is expected at {high_rainy_hours}"
-
+    if high_rainy_hours and moderate_rainy_hours:
+        message = f"Moderate chance of rainfall is expected at hours {moderate_rainy_hours}\n\nHigh chance of rainfall is expected at {high_rainy_hours}"
+    elif moderate_rainy_hours and not high_rainy_hours:
+        message = f"Moderate chance of rainfall is expected at hours {moderate_rainy_hours}"
+    elif high_rainy_hours and not moderate_rainy_hours:
+        message = f"High chance of rainfall is expected at {high_rainy_hours}"
     else:
         message = "No rain expected today"
 
     print(send_message("+254115361123",message))
+main()
 
 schedule.every().day.at("00:00").do(main())
 
